0e4bf322a07bcf5027016d977dad71b1df37e896
cadeclark101 <cadeclark@outlook.com>
2023-01-20 21:57:21 +0000

    Started to try to animate some elements. Want to move singapore map to right side of screen on click.

--- src/Singapore.js
+++ src/Singapore.js
@@ -7,16 +7,17 @@ import Title from './Title';
 import IconButton from './IconButton';
 
 import singapore_map from './img/singapore_map.png';
-import plane_arrival_icon from './img/plane_arrival.png';
+import plane_arrival_icon from './img/plane_arrival.svg';
+import arrow_big_right_lines from './img/arrow_big_right_lines.svg'
 
-function defaultView() {
+function defaultMapView() {
     return (
         <React.Fragment>
             <div id="singapore-map-image-container-default-view">
-                <Map map_location={singapore_map} map_image_height={"75%"} map_image_width={"auto"} image_alt={"map of singapore"} />
+                <Map class="singapore-map-image" map_location={singapore_map} map_image_height={"75%"} map_image_width={"auto"} image_alt={"map of singapore"} />
 
                 <div id="singapore-map-icon-container-default-view">
-                <IconButton icon_location={plane_arrival_icon} />
+                <IconButton id="plane-arrival-icon-button" icon_location={plane_arrival_icon} />
                 </div>
             </div>
 
@@ -27,11 +28,33 @@ function defaultView() {
     )
 }
 
+function activeMapView(){
+    var clicked_button = document.getElementById("plane-arrival-icon-button");
+    var singapore_map_image_container_default_view = document.getElementById("singapore-map-image-container-default-view");
+    clicked_button.onclick = function(){
+        singapore_map.style.visibility = "hidden";
+      }
+}
+
+function airportView() {
+    return (
+        <React.Fragment>
+            <div id="singapore-airport-view-container">
+                <div id="singapore-airport-view-text-container">
+                    <Title main_title={"LONDON"} />
+                    <Title main_title={"ABU DHABI"} />
+                    <Title main_title={"SINGAPORE"} />
+                </div>
+            </div>
+        </React.Fragment>
+    )
+}
+
 
 export default class Singapore extends Component {
     render() {
         return(
-            defaultView()
+            defaultMapView()
         )
         }
 }
