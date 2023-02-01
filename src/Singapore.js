da58a6da89081905b5364e4bbe96066c75519b41
cadeclark101 <cadeclark@outlook.com>
2023-02-01 16:30:55 +0000

    Tweaked fade out/animation functions so the state is set rather than toggled. Fixed the div positioning error. Divs arent amazing in general.

--- src/Singapore.js
+++ src/Singapore.js
@@ -21,26 +21,26 @@ class AirportView extends Component {
     render() {
         return (
             <React.Fragment>
-                <div id="singapore-airport-view-container">
-                    <div id="singapore-airport-view-title-container">
+                <div id="airport-view-container">
+                    <div id="airport-view-title-container">
                         <h1 className='left'>LONDON <img src={uk_flag}></img></h1>
                         <h1 className='center'>ABU DHABI <img src={abu_dhabi_flag}></img></h1>
                         <h1 className='right'>SINGAPORE <img src={singapore_flag}></img></h1>
                     </div>
 
-                    <div id="singapore-airport-view-body-container">
-                        <div id="singapore-airport-view-box-one">
+                    <div id="airport-view-body-container">
+                        <div id="airport-view-box-one">
                             <h2>PRE-DEPARTURE AND PLANNING</h2>
-                            <span className="singapore-airport-hidden-text">
+                            <span className="airport-hidden-text">
                                 <p>A trip that had been on my mind since my first year at the University of Surrey demanded enough planning to make any persons head spin.</p> 
                                 <p>A considerable amount of prerequisites are often overlooked by would-be travellers and social media influencers. </p>
                                 <p>The ability to be <span style={{color: "turquoise"}}>flexible and decisive</span> helped me secure the best possible price for my flight while still maintaining realistic time goals.</p>
                                 <p>A quick sum up of said prerequisites (for entry to Singapore) includes: <span style={{color: "red"}}>COVID-19 documentation, recommended vaccinations, pre-booked residence, and approved airrval card.</span></p>
                             </span>
                         </div>
-                        <div id="singapore-airport-view-box-two">
+                        <div id="airport-view-box-two">
                             <h2>THE SMELLIEST FLIGHT</h2>
-                            <span className="singapore-airport-hidden-text">
+                            <span className="airport-hidden-text">
                                 <p>18 hours is a long time, especially when you are up twelve thousand meters in the air inside a metal can.</p>
                                 <p>It should come at no surprise that if you are planning to come halfway around the world that your initial flight will be long. Luckily, you can take measures to make this experience somewhat bearable without breaking the bank:</p>
                                 <p><span style={{fontSize: "30px", color: "green"}}>FLY WITH A MIDDLE EASTERN AIRLINE.</span></p>
@@ -48,9 +48,9 @@ class AirportView extends Component {
                                 <p>When booking a flight, use multiple cheap-flight websites such as <span style={{color: "red"}}>Skyscanner, Kayak and my personal favourite: Google Flights.</span></p>
                             </span>
                         </div>
-                        <div id="singapore-airport-view-box-three">
+                        <div id="airport-view-box-three">
                             <h2>THE GREATEST AIRPORT IN THE WORLD</h2>
-                            <span className="singapore-airport-hidden-text">
+                            <span className="airport-hidden-text">
                                 <p>Singapore's Changi Airport is considered one of the best airports in the world... for those who depart from it.</p>
                                 <p>Please enjoy this list of features and amenities that I did not get to enjoy:</p>
                                 <ul>
@@ -76,7 +76,7 @@ class TreeTopWalk extends Component {
     render() {
         return(
             <React.Fragment>
-                <div id="singapore-tree-top-walk-container">
+                <div id="tree-top-walk-container">
                     <img src={singapore_jungle}></img>
                 </div>
             </React.Fragment>
@@ -85,11 +85,11 @@ class TreeTopWalk extends Component {
 }
 
 
-
 export default function Singapore() { {
 
     const [showAirportView, setShowAirportView] = useState(true); 
     const [showTreeTopWalkView, setShowTreeTopWalkView] = useState(true);
+    const [showSingaporeMapView, setShowSingaporeMapView] = useState(false);
 
     const wrapperRef = React.useRef();
 
@@ -97,27 +97,30 @@ export default function Singapore() { {
         var wrapper = wrapperRef.current;
         console.log(wrapper);
         wrapper.classList.toggle('animate-slide-right');
+        setShowAirportView(false);
     }
 
     function animateMapFadeOut() {
         var wrapper = wrapperRef.current;
         console.log(wrapper);
         wrapper.classList.toggle('fade-out');
+        setTimeout(function() {
+            setShowTreeTopWalkView(false);
+        }, 500);
     }
 
         return(
             <React.Fragment>
-                <div id="singapore-main-container">
-                    <div ref={wrapperRef} className="main-default-map-view-container">
+                    <div ref={wrapperRef} id="main-default-map-view-container" className="main-default-map-view-container">
 
                         <div id="singapore-map-image-container">
                             <Map id="singapore-map-image" map_location={singapore_map} image_alt={"map of singapore"} />
 
                             <div id="plane-arrival-icon-button-container">
-                                <IconButton id="plane-arrival-icon-button" icon_location={plane_arrival_icon} onClick={() => { setShowAirportView(s => !s); animateMapRight();}}/>
+                                <IconButton id="plane-arrival-icon-button" icon_location={plane_arrival_icon} onClick={() => { animateMapRight();}}/>
                             </div>
                             <div id="tree-icon-button-container">
-                                <IconButton id="tree-icon-button" icon_location={tree_icon} onClick={() => { setShowTreeTopWalkView(s => !s); animateMapFadeOut();}}/>
+                                <IconButton id="tree-icon-button" icon_location={tree_icon} onClick={() => { animateMapFadeOut();}}/>
                             </div>
                             
                         </div>
@@ -127,6 +130,7 @@ export default function Singapore() { {
                         </div>
 
                     </div>
+
                     <div id="airport-main-container">
                         {!showAirportView ? <AirportView /> : null}
                     </div>
@@ -135,7 +139,7 @@ export default function Singapore() { {
                         {!showTreeTopWalkView ? <TreeTopWalk /> : null}
                     </div>
                     
-                </div>
+
             </React.Fragment>
         )
         }
