a703619dcea7908c3101e11209867ebf4cb65193
cadeclark101 <cadeclark@outlook.com>
2023-02-22 12:04:35 +0000

    Finally fixed the resize map function - could be more accurate. Started to add some actual content to Chinatown section

--- src/Airport.js
+++ src/Airport.js
@@ -22,27 +22,27 @@ export default class AirportView extends Component {
                         <div id="box-one">
                             <h2>PRE-DEPARTURE AND PLANNING</h2>
                             <span className="hidden-text">
-                                <p>A trip that had been on my mind since my first year at the University of Surrey demanded enough planning to make any persons head spin.</p> 
-                                <p>A considerable amount of prerequisites are often overlooked by would-be travellers and social media influencers. </p>
-                                <p>The ability to be <span style={{color: "turquoise"}}>flexible and decisive</span> helped me secure the best possible price for my flight while still maintaining realistic time goals.</p>
-                                <p>A quick sum up of said prerequisites (for entry to Singapore) includes: <span style={{color: "red"}}>COVID-19 documentation, recommended vaccinations, pre-booked residence, and approved airrval card.</span></p>
+                                <p className='airport-text'>A trip that had been on my mind since my first year at the University of Surrey demanded enough planning to make any persons head spin.</p> 
+                                <p className='airport-text'>A considerable amount of prerequisites are often overlooked by would-be travellers and social media influencers. </p>
+                                <p className='airport-text'>The ability to be <span style={{color: "turquoise"}}>flexible and decisive</span> helped me secure the best possible price for my flight while still maintaining realistic time goals.</p>
+                                <p className='airport-text'>A quick sum up of said prerequisites (for entry to Singapore) includes: <span style={{color: "red"}}>COVID-19 documentation, recommended vaccinations, pre-booked residence, and approved airrval card.</span></p>
                             </span>
                         </div>
                         <div id="box-two">
                             <h2>THE SMELLIEST FLIGHT</h2>
                             <span className="hidden-text">
-                                <p>18 hours is a long time, especially when you are up twelve thousand meters in the air inside a metal can.</p>
-                                <p>It should come at no surprise that if you are planning to come halfway around the world that your initial flight will be long. Luckily, you can take measures to make this experience somewhat bearable without breaking the bank:</p>
-                                <p><span style={{fontSize: "30px", color: "green"}}>FLY WITH A MIDDLE EASTERN AIRLINE.</span></p>
-                                <p>I flew with <span style={{ color: "gold"}}>Etihad Airways</span> for both legs of the journey and both were bearable compared to the atrocities I would have faced on any of the European airlines.</p>
-                                <p>When booking a flight, use multiple cheap-flight websites such as <span style={{color: "red"}}>Skyscanner, Kayak and my personal favourite: Google Flights.</span></p>
+                                <p className='airport-text'>18 hours is a long time, especially when you are up twelve thousand meters in the air inside a metal can.</p>
+                                <p className='airport-text'>It should come at no surprise that if you are planning to come halfway around the world that your initial flight will be long. Luckily, you can take measures to make this experience somewhat bearable without breaking the bank:</p>
+                                <p className='airport-text'><span style={{fontSize: "30px", color: "green"}}>FLY WITH A MIDDLE EASTERN AIRLINE.</span></p>
+                                <p className='airport-text'>I flew with <span style={{ color: "gold"}}>Etihad Airways</span> for both legs of the journey and both were bearable compared to the atrocities I would have faced on any of the European airlines.</p>
+                                <p className='airport-text'>When booking a flight, use multiple cheap-flight websites such as <span style={{color: "red"}}>Skyscanner, Kayak and my personal favourite: Google Flights.</span></p>
                             </span>
                         </div>
                         <div id="box-three">
                             <h2>THE GREATEST AIRPORT IN THE WORLD</h2>
                             <span className="hidden-text">
-                                <p>Singapore's Changi Airport is considered one of the best airports in the world... for those who depart from it.</p>
-                                <p>Please enjoy this list of features and amenities that I did not get to enjoy:</p>
+                                <p className='airport-text'>Singapore's Changi Airport is considered one of the best airports in the world... for those who depart from it.</p>
+                                <p className='airport-text'>Please enjoy this list of features and amenities that I did not get to enjoy:</p>
                                 <ul>
                                     <li>Butterfly garden,</li>
                                     <li>Four-story slide,</li>
--- src/Chinatown.js
+++ src/Chinatown.js
@@ -3,38 +3,13 @@ import 'jquery';
 
 import './Chinatown.css';
 
+
 import noodle_stand_image from './img/singapore/chinatown/noodle_stand_vector.svg';
+import Title from './Title';
 
 
 
 export default class Chinatown extends Component {
-
-    resizeMaps() {
-        var org_width = 650;
-        var org_height = 510;
-
-        var rendered_width = document.getElementById("noodle-stand-image").clientWidth;
-        var rendered_height = document.getElementById("noodle-stand-image").clientHeight;
-
-        var widthRatio = rendered_width / org_width;
-        var heightRatio = rendered_height / org_height;
-
-        var areas = document.getElementsByTagName("area");
-
-        for (var i = 0; i < areas.length; i++) {
-            var new_coords = [];
-            var coords_split =  areas[i].coords.split(",");
-            for (var a = 0; a < coords_split.length; a += 2) {
-                new_coords.push(coords_split[a] * widthRatio);
-                new_coords.push(coords_split[a + 1] * heightRatio);
-            }
-            coords_split = [];
-            areas[i].coords = new_coords;
-            console.log(areas[i].coords)
-        }
-    }
-
-    
 
     render() {
         
@@ -46,16 +21,57 @@ export default class Chinatown extends Component {
                     <div id="noodle-stand-image-container">
                         <img id="noodle-stand-image" src={noodle_stand_image} border="0" orgwidth="650" orgheight="510" useMap="#image-maps-2023-02-14-105000" alt="" />
                             <map name="image-maps-2023-02-14-105000" id="ImageMapsCom-image-maps-2023-02-14-105000">
-                                <area alt="" href="#1" title="Test" shape="poly" coords="256,114,429,96,414,276,399,273,396,242,390,249,387,266,376,264,352,258,354,269,282,264,283,251,246,248" target="_self" />
-                                <area alt="" href="#2" title="" shape="poly" coords="245,48,489,0,540,1,534,86,243,112" target="_self" />
-                                <area alt="" href="#3" title=""  shape="poly" coords="429,97,531,88,512,286,412,278"  target="_self" />
-                                <area alt="" href="#4" title="" shape="poly" coords="216,112,208,248,244,246,254,118,230,120" target="_self" />
+                                <area alt="" className="test" href="#1" title="1" shape="poly" style={{outline:"1px solid black"}} coords="256,114,429,96,414,276,399,273,396,242,390,249,387,266,376,264,352,258,354,269,282,264,283,251,246,248" target="_self" />
+                                <area alt="" href="#2" title="2" shape="poly" style={{outline:"1px solid red"}} coords="245,48,489,0,540,1,534,86,243,112" target="_self" />
+                                <area alt="" href="#3" title="3"  shape="poly" style={{outline:"1px solid green"}} coords="429,97,531,88,512,286,412,278"  target="_self" />
+                                <area alt="" href="#4" title="4" shape="poly" style={{outline:"1px solid white"}} coords="216,112,208,248,244,246,254,118,230,120" target="_self" />
                             </map>
                     </div>
-                    <button onClick={this.resizeMaps}></button>
+                    <div id="chinatown-title-container">
+                        <p className="placeholder-text-title">WESTERN, MALAYSIAN, CHINESE, INDIAN, SINGAPOREAN</p>
+                    </div>
                 </div>
             </React.Fragment>
         )
     }
+
+    resizeMaps = () => {
+        var areas = document.getElementsByTagName("area");
+
+        areas[0].coords = "256,114,429,96,414,276,399,273,396,242,390,249,387,266,376,264,352,258,354,269,282,264,283,251,246,248";
+        areas[1].coords = "245,48,489,0,540,1,534,86,243,112";
+        areas[2].coords = "429,97,531,88,512,286,412,278";
+        areas[3].coords = "216,112,208,248,244,246,254,118,230,120";
+
+        var org_width = 650;
+        var org_height = 510;
+
+        var rendered_width = document.getElementById("noodle-stand-image").clientWidth;
+        var rendered_height = document.getElementById("noodle-stand-image").clientHeight;
+
+        var widthRatio = rendered_width / org_width;
+        var heightRatio = rendered_height / org_height;
+
+        for (var i = 0; i < areas.length; i++) {
+            var new_coords = [];
+            var coords_split =  areas[i].coords.split(",");
+            for (var a = 0; a < coords_split.length; a += 2) {
+                new_coords.push(coords_split[a] * widthRatio);
+                new_coords.push(coords_split[a + 1] * heightRatio);
+            }
+            coords_split = [];
+            areas[i].coords = new_coords;
+            console.log(areas[i].coords);
+        }
+    }
+
+    componentDidMount() {
+        this.resizeMaps()
+        window.addEventListener('resize', this.resizeMaps);
+      }
+      componentWillUnmount() {
+        window.removeEventListener('resize', this.resizeMaps);
+      }
+
 }
 
